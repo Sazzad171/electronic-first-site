00e47f653038607618abe3340a960a852e2319cf
Sazzad171 <sajjad.cse.diu@gmail.com>
2023-08-21 21:42:56 +0600

    update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
-export default {
-  build: {
-    rollupOptions: {
-      input: {
-        main: 'index.html'
-      }
-    }
-  }
-}
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+export default defineConfig({
+    build: {
+        outDir: 'dist',
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                fetch: resolve(__dirname, 'fetch.html'),
+                key: resolve(__dirname, 'filename.html'),
+
+            }
+        }
+    },
+});
